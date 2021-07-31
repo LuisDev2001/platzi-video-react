99b342dcad5925545a355093dbc323e44116c6b4
LuiDev2001 <luisqrja@gmail.com>
2021-07-30 21:34:21 -0500

    Add validation for login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,63 @@
 import React from "react";
-import "../assets/styles/components/Header.scss";
-
-import logoPlatziVideo from "../assets/img/logo-platzi-video-BW2.png";
-import userIcon from "../assets/img/user-icon.png";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import gravatar from "../utils/gravatar";
+import "../assets/styles/components/Header.scss";
+import logo from "../assets/img/logo-platzi-video-BW2.png";
+import userIcon from "../assets/img/user-icon.png";
 
-const Header = () => (
-  <header className="header">
-    <Link to="/">
-      <img className="header__img" src={logoPlatziVideo} alt="Platzi Video" />
-    </Link>
-    <div className="header__menu">
-      <div className="header__menu--profile">
-        <img src={userIcon} alt="" />
-        <p>Perfil</p>
+const Header = (props) => {
+  console.log(props);
+  const { user } = props;
+  const hasUser = Object.keys(user).length > 0;
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+  return (
+    <header className="header">
+      <Link to="/">
+        <img className="header__img" src={logo} alt="Platzi Video" />
+      </Link>
+      <div className="header__menu">
+        <div className="header__menu--profile">
+          {hasUser ? (
+            <img src={gravatar(user.email)} alt={user.email} />
+          ) : (
+            <img src={userIcon} alt="User icon" />
+          )}
+          <p>Perfil</p>
+        </div>
+        <ul>
+          {hasUser ? (
+            <li>
+              <Link to="/">{user.name}</Link>
+            </li>
+          ) : null}
+          {hasUser ? (
+            <li>
+              <Link to="/" onClick={handleLogout}>
+                Cerrar Sesión
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link to="/login">Iniciar Sesión</Link>
+            </li>
+          )}
+        </ul>
       </div>
-      <ul>
-        <li>
-          <a href="/">Cuenta</a>
-        </li>
-        <li>
-          <Link to="/login">Iniciar sesión</Link>
-        </li>
-        <li>
-          <a href="/">Cerrar Sesión</a>
-        </li>
-      </ul>
-    </div>
-  </header>
-);
+    </header>
+  );
+};
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  };
+};
+
+// const mapDispatchToProps = {
+//   logoutRequest,
+// };
+
+export default connect(mapStateToProps, null)(Header);
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,71 +1,80 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
-import "../assets/styles/components/Login.scss";
 import { Link } from "react-router-dom";
 import { loginRequest } from "../actions";
-//Images
 import googleIcon from "../assets/img/google-icon.png";
 import twitterIcon from "../assets/img/twitter-icon.png";
+
+import "../assets/styles/components/Login.scss";
 
 const Login = (props) => {
   const [form, setValues] = useState({
     email: "",
   });
 
-  const handleInput = (event) => {
+  const handleInput = (e) => {
     setValues({
       ...form,
-      [event.target.name]: event.target.value,
+      [e.target.name]: e.target.value,
     });
   };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
     props.loginRequest(form);
     props.history.push("/");
   };
 
   return (
-    <section className="login">
-      <section className="login__container">
-        <h2>Inicia sesión</h2>
-        <form className="login__container--form" onSubmit={handleSubmit}>
-          <input
-            name="email"
-            className="input"
-            type="text"
-            placeholder="Correo"
-            onChange={handleInput}
-          />
-          <input
-            name="password"
-            className="input"
-            type="password"
-            placeholder="Contraseña"
-            onChange={handleInput}
-          />
-          <button className="button">Iniciar sesión</button>
-          <div className="login__container--remember-me">
-            <label>
-              <input type="checkbox" id="cbox1" value="first_checkbox" />
-              Recuérdame
-            </label>
-            <a href="/">Olvidé mi contraseña</a>
-          </div>
-        </form>
-        <section className="login__container--social-media">
-          <div>
-            <img src={googleIcon} /> Inicia sesión con Google
-          </div>
-          <div>
-            <img src={twitterIcon} /> Inicia sesión con Twitter
-          </div>
+    <>
+      <section className="login">
+        <section className="login__container">
+          <h2>Inicia sesión</h2>
+          <form className="login__container--form" onSubmit={handleSubmit}>
+            <input
+              name="email"
+              className="input"
+              type="text"
+              placeholder="Correo"
+              autoComplete="username"
+              onChange={handleInput}
+            />
+            <input
+              name="password"
+              className="input"
+              type="password"
+              placeholder="Contraseña"
+              autoComplete="current-password"
+              onChange={handleInput}
+            />
+            <button className="button">Iniciar sesión</button>
+            <div className="login__container--remember-me">
+              <label>
+                <input
+                  type="checkbox"
+                  id="cbox1"
+                  defaultValue="first_checkbox"
+                />
+                Recuérdame
+              </label>
+              <a href="/">Olvidé mi contraseña</a>
+            </div>
+          </form>
+          <section className="login__container--social-media">
+            <div>
+              <img src={googleIcon} /> Inicia sesión con Google
+            </div>
+            <div>
+              <img src={twitterIcon} /> Inicia sesión con Twitter
+            </div>
+          </section>
+          <p className="login__container--register">
+            No tienes ninguna cuenta {"  "}
+            <Link to="/register">Regístrate</Link>
+          </p>
         </section>
-        <p className="login__container--register">
-          No tienes ninguna cuenta <Link to="/register">Regístrate</Link>
-        </p>
       </section>
-    </section>
+    </>
   );
 };
 
